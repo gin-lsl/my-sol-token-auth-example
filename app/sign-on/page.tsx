bf2ca10b70617bf4ba59ea75ec8a48be3b6c1eeb
gin-lsl <gin_lsl@outlook.com>
2024-11-14 09:40:08 +0800

    fix: error sign button

--- app/sign-on/page.tsx
+++ app/sign-on/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const SignInButton = dynamic(() => import("../sign-in/sign-button"), {
+const SignInButton = dynamic(() => import("./sign-button"), {
   ssr: false,
 });
 
